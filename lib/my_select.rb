11a88abf706116e58651c76cec7b6c870868f363
Jake L <jakelovitz@gmail.com>
2019-04-05 14:27:00 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- 
+   i = 0
+  new_array = []
+  new_array2 = []
+  while i < collection.length
+    new_array.push(collection[i])
+    yield(new_array[i])
+    new_array2.push(yield(new_array[i]))
+    i = i + 1
+  end
+  new_array2
 end
