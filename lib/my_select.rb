e4e1cdded4c5fa18713d0a5a522e151bfb5e47f1
Jake L <jakelovitz@gmail.com>
2019-04-05 15:02:31 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,10 @@
 def my_select(collection)
   i = 0
+  new_collection = []
   while i < collection.length
-    if collection[i] == 
-    yield(collection[i])
+    if collection[i].even?
+      new_collection.push(yield(collection[i]))
     i = i + 1
   end
-  collection
+  new_collection
 end
