ad008a8c16a1a89c5b9292a3fa3bc68b63035ebb
Jake L <jakelovitz@gmail.com>
2019-04-05 14:28:21 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,8 @@
 def my_select(collection)
-   i = 0
-  while i < collection.length
-    new_array.push(collection[i])
-    yield(new_array[i])
-    new_array2.push(yield(new_array[i]))
+  i = 0
+  while i < array.length
+    yield(array[i])
     i = i + 1
   end
-  new_array2
+  array
 end
